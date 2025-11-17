384fc1f12663b081abb5a54fde9e4cab3d0db613
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2025-11-16 21:21:16 -0800

    Initial implementation of `DNAString` and `DNAStringSet` (#1)

--- docs/conf.py
+++ docs/conf.py
@@ -299,6 +299,7 @@ intersphinx_mapping = {
     "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
     "setuptools": ("https://setuptools.pypa.io/en/stable/", None),
     "pyscaffold": ("https://pyscaffold.org/en/stable", None),
+    "iranges": ("https://biocpy.github.io/IRanges", None),
 }
 
 print(f"loading configurations for {project} {version} ...", file=sys.stderr)
